37d8ce98747b97f06bc24a37373f697f66152d4e
Alastair Paragas <alastairparagas@gmail.com>
2015-07-06 02:26:02 -0400

    Auth and Missing Route middlewares

---
+++ cubletApi/middleware/auth.js
@@ -0,0 +1,10 @@
+(function () {
+	'use strict';
+	
+	function auth(req, res, next) {
+		
+	}
+	
+	module.exports = auth;
+	
+}());
---
+++ cubletApi/middleware/missing.js
@@ -0,0 +1,22 @@
+(function () {
+	'use strict';
+	
+	var apiView = require('cubletApi/apiView');
+	
+	/**
+	* Middleware for handling missing routes.
+	* @module cubletApi
+	* @name missing
+	* @param {Object} req - Express Request Object
+	* @param {Object} res - Express Response Object
+	*/
+	function missingMiddleware(req, res) {
+		apiView(res, {
+			status: 404,
+			message: 'API endpoint does not exist'
+		});
+	}
+	
+	module.exports = missingMiddleware;
+	
+}());
