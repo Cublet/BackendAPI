314dfaff16588265e015f98e8b17dec70baf8af1
Alastair Paragas <alastairparagas@gmail.com>
2015-07-07 17:34:45 -0400

    Complete Auth/JWT middleware implementation

--- cubletApi/middleware/auth.js
+++ cubletApi/middleware/auth.js
@@ -1,8 +1,38 @@
 (function () {
 	'use strict';
 	
+	var apiView = require('cubletApi/apiView'), 
+		authToken = require('cubletApi/auth/authToken');
+	
+	/**
+	* Middleware for handling authentication. On verification of token, 
+	* the decoded payload is placed upon req.authToken.
+	* @param {Object} req - Express Request Object
+	* @param {Object} res - Express Response Object
+	* @param {Function} next - Express callback to continue running
+	*/
 	function auth(req, res, next) {
+		var authHeaders = req.get('Authorization'),
+			tokenString = authHeaders && authHeaders.split(" ")[1],
+			decodedAuthToken;
 		
+		if (!authHeaders || !authToken) {
+			return apiView(res, {
+				status: 403,
+				message: 'Unauthorized'
+			});
+		}
+		
+		decodedAuthToken = authToken.decode(tokenString);
+		if (!decodedAuthToken) {
+			return apiView(res, {
+				status: 403,
+				message: 'Invalid Token'
+			});
+		}
+		
+		req.authToken = decodedAuthToken;
+		next();
 	}
 	
 	module.exports = auth;
