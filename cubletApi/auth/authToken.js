9e06938c7b565d1e559f9af7bfe6faef724b4085
Alastair Paragas <alastairparagas@gmail.com>
2015-07-07 18:25:26 -0400

    JSON Web Token Encode/Decode Module

---
+++ cubletApi/auth/authToken.js
@@ -0,0 +1,41 @@
+(function () {
+	'use strict';
+	
+	var jwt = require('jsonwebtoken'),
+		config = require('config');
+	
+	/**
+	* @module cubletApi/auth
+	* @function generate
+	* @param {{Object|Buffer|string}} payload - The information to encrypt
+	* @returns {string} Encrypted token
+	*/
+	function generate(payload) {
+		return jwt.sign(payload, config.jwtSecret, {
+			expiresInMinutes: 1440
+		});
+	}
+	
+	/**
+	* @module cubletApi/auth
+	* @function decode
+	* @param {string} tokenString - The token to decode
+	* @returns {?(Object|Buffer|string)} Decoded information, null if invalid
+	*/
+	function decode(tokenString) {
+		var decoded;
+		
+		try {
+			decoded = jwt.verify(tokenString, config.jwtSecret);
+		} catch (err) {
+			decoded = null;
+		}
+		return decoded;
+	}
+	
+	module.exports = {
+		generate: generate,
+		decode: decode
+	};
+	
+}());
