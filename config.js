c6dd6f5656582ba33fce9870b7cab1ea41b528ee
Alastair Paragas <alastairparagas@gmail.com>
2015-07-12 20:11:38 -0400

    Config file

---
+++ config.js
@@ -0,0 +1,33 @@
+(function (global) {
+	'use strict';
+	
+	/**
+	* Configuration File for Cublet's API Backend
+	* The following must be setup by you, manually, in any environment you 
+	* choose to deploy Cublet/BackendAPI in. This file manages holding secrets
+	* like app keys, customizable welcome messages and other sorts of settings.
+	*
+	* It is important that you try to keep this file as private as possible.
+	*/
+
+	var packageInfo = require('./package.json'),
+
+		config = {
+			versionNumber: packageInfo.version.split('.')[0],
+			portNumber: 8000,
+			greeterMessage: 'Welcome to Cublet\'s API. For more information, ' + 
+			'check out https://github.com/Cublet/BackendAPI',
+			Mongoose: {
+				debug: true,
+				connectionUri: ''
+			},
+			jwtSecret: '',
+			facebook: {
+				appId: '',
+				appSecret: ''
+			}
+		};
+
+	module.exports = config;
+
+}(global));
